7653fcde761b96f9c1312c3a131ef3e3126835f3
s14228so <saongtx7@gmail.com>
2019-10-05 16:57:39 +0900

    ok

--- pages/beers/index.vue
+++ pages/beers/index.vue
@@ -1,6 +1,54 @@
 <template>
   <div class="container">
+    
     <h3>beers</h3>
+
+
+
+
+
+
+
+
+
+
+    <table class="table table-responsive-sm table-hover table-outline mb-0">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>ビール名</th>
+                        <th>値段</th>
+                        <th>アルコール度数</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                       <tr class="lists" v-for="(beer, index) in beers" :key="index">
+                        <td class="text-center">
+                          <div>{{beer.title}}</div>
+                          <div class="small text-muted">
+                            <span>New</span> | Registered: Jan 1, 2015</div>
+                        </td>
+                        <td>
+                          <div>{{beer.price}}円</div>
+                          <div class="small text-muted">
+                            <span>New</span> | Registered: Jan 1, 2015</div>
+                        </td>
+                        <td>
+                          <div class="clearfix">
+                            <div class="float-left">
+                              <strong>{{beer.alcohol}}</strong>
+                            </div>
+                          </div>
+                          <div class="progress progress-xs">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 50%" :aria-valuenow="beer.alcohol" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+
+
+                  
     <form @submit.prevent="handleSubmit">
       <div>
         <label for>ビール名</label>
@@ -16,7 +64,7 @@
       </div>
       <input type="submit" value="追加" />
     </form>
-    <li class="lists" v-for="(beer, index) in beers" :key="index" :class="{ active: beer.isActive}">
+    <li class="lists" v-for="(beer, index) in beers" :key="index">
       id: {{beer.id}}
       商品名: {{beer.title}}
       値段: {{beer.price}}円
@@ -44,11 +92,12 @@ export default {
     const { data } = await axios.get(`${process.env.API}/beers`);
 
     this.beers = data.Items.sort((a, b) => (a.id > b.id ? 1 : -1));
+          console.log(this.beers);
   },
   methods: {
     async handleSubmit() {
       console.log("aaa");
-      console.log(this.alcohol);
+
       const { data } = await axios.post(`${process.env.API}/beers`, {
         id: this.beers.length + 1,
         title: this.title,
---
+++ pages/chart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h3>Home</h3>
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    data(){
+        return{
+            
+        }
+    },
+   async created(){
+    console.log(process.env.API)
+    const { data } = await axios.get(`${process.env.API}/beers`);
+    this.beers = data.Items.sort((a, b) => (a.id > b.id ? 1 : -1));
+   }
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+1
